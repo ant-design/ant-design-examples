d613c5a4dba8c3f06da6d7882557f09b30aacb58
kiner-tang(文辉) <1127031143@qq.com>
2023-07-15 16:12:39 +0800

    feat: remove styled-components

--- examples/with-nextjs-app-router-inline-style/src/lib/registry.tsx
+++ examples/with-nextjs-app-router-inline-style/src/lib/registry.tsx
@@ -1,9 +1,8 @@
 
 'use client'
 
-import React, { useState } from 'react';
+import React from 'react';
 import { useServerInsertedHTML } from 'next/navigation';
-import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs';
 
 
@@ -12,25 +11,17 @@ export default function StyledComponentsRegistry({
 }: {
     children: React.ReactNode
 }) {
-    // Only create stylesheet once with lazy initial state
-    // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
-    const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
     const cache = createCache();
 
     useServerInsertedHTML(() => {
-        const styles = styledComponentsStyleSheet.getStyleElement()
-        styledComponentsStyleSheet.instance.clearTag()
         return <>
             <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }}></style>
-            {styles}
         </>
     })
 
     return (
-        <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-            <StyleProvider cache={cache}>
-                {children}
-            </StyleProvider>
-        </StyleSheetManager>
+        <StyleProvider cache={cache}>
+            {children}
+        </StyleProvider>
     )
 }
