dfbb32dccb62933c411683b408850f09a814f100
MadCcc <1075746765@qq.com>
2023-12-29 10:26:28 +0800

    feat: use @ant-design/nextjs-registry

--- examples/with-nextjs-app-router-inline-style/src/app/layout.tsx
+++ examples/with-nextjs-app-router-inline-style/src/app/layout.tsx
@@ -1,6 +1,7 @@
-import StyledComponentsRegistry from '../lib/AntdRegistry'
 import './globals.css'
+import React from 'react';
 import { Inter } from 'next/font/google'
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
   )
